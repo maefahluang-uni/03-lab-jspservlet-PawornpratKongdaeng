9b3ea7f07d7abe84ae299652a09908e82a10a8ba
PawornpratKongdaeng <43003179+PawornpratKongdaeng@users.noreply.github.com>
2023-09-04 15:44:31 +0700

    Enter

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -1,28 +1,66 @@
 package th.mfu;
 
 import java.io.IOException;
-
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
-public class BMICalculatorServlet extends HttpServlet{
+public class BMICalculatorServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
-        
-        //TODO: calculate bmi
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Get parameters from the request: "weight" and "height"
+        String weightParam = request.getParameter("weight");
+        String heightParam = request.getParameter("height");
 
-        //TODO: determine the built from BMI
-      
-        //TODO: add bmi and built to the request's attribute
+        // Check if either parameter is missing or empty
+        if (weightParam == null || weightParam.isEmpty() || heightParam == null || heightParam.isEmpty()) {
+            // You may want to handle the error here, e.g., by displaying a message or redirecting to an error page.
+            response.getWriter().println("Please provide valid weight and height parameters.");
+            return;
+        }
 
-        //TODO: forward to jsp
-           
+        try {
+            // Convert parameters to numerical values
+            double weight = Double.parseDouble(weightParam);
+            double height = Double.parseDouble(heightParam);
+
+            // Calculate BMI
+            double bmi = calculateBMI(weight, height);
+
+            // Determine the body type from BMI (you can customize this logic)
+            String bodyType = determineBodyType(bmi);
+
+            // Add BMI and body type to the request's attributes
+            request.setAttribute("bmi", bmi);
+            request.setAttribute("bodyType", bodyType);
+
+            // Forward to a JSP for rendering
+            request.getRequestDispatcher("/result.jsp").forward(request, response);
+        } catch (NumberFormatException e) {
+            // Handle invalid input format
+            response.getWriter().println("Invalid weight or height format. Please provide valid numerical values.");
+        }
     }
-    
+
+    private double calculateBMI(double weight, double height) {
+        // Calculate BMI (simplified formula)
+        return weight / (height * height);
+    }
+
+    private String determineBodyType(double bmi) {
+        // Customize this logic based on your criteria
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            return "Normal Weight";
+        } else if (bmi >= 25 && bmi < 29.9) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
 }
